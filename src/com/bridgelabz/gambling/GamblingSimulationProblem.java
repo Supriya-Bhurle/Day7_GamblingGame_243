ebdd193ce9f507e4ae4b7b4de90bb97b9892992d
Supriya Bhurle <supriyabhurle28@gmail.com>
2023-01-24 09:51:29 +0530

    Added:Gambling Simulation Program

--- src/com/bridgelabz/gambling/GamblingSimulationProblem.java
+++ src/com/bridgelabz/gambling/GamblingSimulationProblem.java
@@ -1,2 +1,7 @@
-package com.bridgelabz.gambling;public class GamblingSimulationProblem {
+package com.bridgelabz.gambling;
+
+public class GamblingSimulationProblem {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Gambling Simulation Program");
+    }
 }
