3bd93d6e7423f45c828d796e698e6624bac4b2fb
Supriya Bhurle <supriyabhurle28@gmail.com>
2023-01-24 09:57:54 +0530

    Added:Gambler start with initial stake

--- src/com/bridgelabz/gambling/GamblingSimulationProblem.java
+++ src/com/bridgelabz/gambling/GamblingSimulationProblem.java
@@ -3,5 +3,8 @@ package com.bridgelabz.gambling;
 public class GamblingSimulationProblem {
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Simulation Program");
+        int stake = 100;
+        int bet = 1;
+        System.out.println("start with a stake of " + stake +" every day and bet " + bet +" every game.");
     }
 }
